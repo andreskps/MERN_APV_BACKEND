1ef500a1b5ec98e000fe9a76790f0a60e1e81e08
andreskps <pipesierra146@gmail.com>
2023-01-21 15:35:08 -0500

    config 2

--- helpers/emailOlvidePassword.js
+++ helpers/emailOlvidePassword.js
@@ -6,6 +6,7 @@ const emailOlvidePassword = async (datos) => {
     const transport = nodemailer.createTransport({
         host: process.env.EMAIL_HOST,
         port: process.env.EMAIL_PORT,
+        secure: true, // true for 465, false for other ports
         auth: {
           user: process.env.EMAIL_USER,
           pass: process.env.EMAIL_PASS
